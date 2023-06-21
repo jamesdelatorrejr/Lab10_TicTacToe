3678e42b6623f2b0b9f398dc4e92d838059fe175
JamesyLive <jamesjrbus@gmail.com>
2023-06-20 21:13:08 -0400

    Commit 2

--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,2 +1,235 @@
-package PACKAGE_NAME;public class SafeInpout {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+public class SafeInput {
+    /**
+     * @param pipe   a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt) {
+        String retString = "";  // Set this to zero length. Loop runs until it isn’t
+        do {
+            System.out.print("\n" + prompt + ": "); // show prompt add space
+            retString = pipe.nextLine();
+        } while (retString.length() == 0);
+
+        return retString;
+
+    }
+
+    /**
+     * @param pipe   a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return an int and ensure that it is an int
+     */
+    public static int getInt(Scanner pipe, String prompt) {
+        int userInt = 0;
+        boolean done = false;
+        do {
+            System.out.println("\n" + prompt + ": ");
+            if (pipe.hasNextInt()) {
+                userInt = pipe.nextInt();
+                done = true;
+            } else {
+                System.out.println("Please enter an int.");
+                done = false;
+                pipe.nextLine();
+            }
+        } while (!done);
+
+        return userInt;
+
+    }
+
+    /**
+     * @param pipe   a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a double and ensure that it is a double
+     */
+    public static double getDouble(Scanner pipe, String prompt) {
+        double userDouble = 0;
+        boolean done = false;
+        do {
+            System.out.println("\n" + prompt + ": ");
+            if (pipe.hasNextDouble()) {
+                userDouble = pipe.nextDouble();
+                done = true;
+            } else {
+                System.out.println("Please enter an double.");
+                done = false;
+                pipe.nextLine();
+            }
+        } while (!done);
+
+        return userDouble;
+
+    }
+
+    /**
+     * @param pipe   a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @param lo     low range for int
+     * @param hi     high range for int
+     * @return an int and ensure that it is an int
+     */
+    public static int getRangedInt(Scanner pipe, String prompt, int lo, int hi) {
+        int userInt = 0;
+        boolean done = false;
+        do {
+            System.out.println("\n" + prompt + ": ");
+            if (pipe.hasNextInt()) {
+                userInt = pipe.nextInt();
+                done = true;
+            } else {
+                System.out.println("Please enter an int.");
+                done = false;
+                pipe.nextLine();
+            }
+            if (userInt >= lo && userInt <= hi && done) {
+                System.out.println("You have entered a number " + userInt + " that is in range.");
+            } else if (!done) {
+
+            } else {
+                done = false;
+                System.out.println("Number " + userInt + " is not in range. Please pick a number between " + lo + "-" + hi);
+            }
+        } while (!done);
+
+        return userInt;
+
+    }
+
+    /**
+     * @param pipe   a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @param lo     low range for double
+     * @param hi     high range for double
+     * @return a double and ensure that it is a double
+     */
+    public static double getRangedDouble(Scanner pipe, String prompt, double lo, double hi) {
+        double userDouble = 0;
+        boolean done = false;
+        do {
+            System.out.println("\n" + prompt + ": ");
+            if (pipe.hasNextDouble()) {
+                userDouble = pipe.nextDouble();
+                done = true;
+            } else {
+                System.out.println("Please enter a double.");
+                done = false;
+                pipe.nextLine();
+            }
+            if (userDouble >= lo && userDouble <= hi && done) {
+                System.out.println("You have entered a number " + userDouble + " that is in range.");
+            } else if (!done) {
+
+            } else {
+                done = false;
+                System.out.println("Number " + userDouble + " is not in range. Please pick a number between " + lo + "-" + hi);
+            }
+        } while (!done);
+
+        return userDouble;
+    }
+
+    /**
+     * @param pipe   a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a boolean true for Y, false for N
+     */
+    public static boolean getYNConfirm(Scanner pipe, String prompt) {
+        boolean userYN = false;
+        String userInt = "";
+        boolean done = false;
+        do {
+            System.out.println("\n" + prompt + ": ");
+            if (pipe.hasNextLine()) {
+                userInt = pipe.nextLine();
+            } else {
+                System.out.println("Please enter a string.");
+                pipe.nextLine();
+            }
+            if (userInt.equalsIgnoreCase("Y")) {
+                userYN = true;
+                done = true;
+            } else if (userInt.equalsIgnoreCase("N")) {
+                userYN = false;
+                done = true;
+
+            } else {
+                System.out.println("There is an error in your input, please put a Y/N.");
+                done = false;
+            }
+
+        } while (!done);
+
+        return userYN;
+    }
+
+    /**
+     * @param pipe   a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @param regEx  string given to match
+     * @return a String equal or not equal to the regex string
+     */
+    public static String getRegExString(Scanner pipe, String prompt, String regEx) {
+        String userString = "";
+        boolean done = false;
+        boolean done1 = false;
+        do {
+            do {
+                System.out.println("\n" + prompt + ": ");
+                if (pipe.hasNextLine()) {
+                    userString = pipe.nextLine();
+                    done = true;
+                } else {
+                    System.out.println("Please enter a string.");
+                    pipe.nextLine();
+                }
+            } while (!done);
+
+            Pattern pattern = Pattern.compile(regEx, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(userString);
+            done = false;
+
+            boolean userEqRegEx = matcher.find();
+
+
+            if (userEqRegEx == true) {
+                System.out.println(regEx + " pattern was found.");
+                done1 = true;
+            } else {
+                System.out.println("The pattern " + regEx + " was not found.");
+            }
+        } while (!done1);
+
+        return userString;
+    }
+
+
+    /**
+     * @param msg       a Scanner opened to read from System.in
+     * @return a String equal or not equal to the regex string
+     */
+    static String PrettyHeader(String msg) {
+        int lineLength = 60;
+        String formattedMsg = "";
+        int msgLength = msg.length();
+        int spaceLength = (lineLength - msgLength - 6) / 2;
+        String space = "";
+        String starLine = "";
+
+        for (int i = 0; i < spaceLength+1; i++) {
+            space += " ";
+        }
+
+        for (int i = 0; i < lineLength+1; i++) {
+            starLine += "*";
+        }
+
+        formattedMsg += starLine + "\n" + "***" + space + msg + space + "***" + "\n" + starLine;
+
+        return formattedMsg;
+    }
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,17 +1,124 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import java.util.Scanner;
+
+public class TicTacToe {
+    private static final int ROW = 3;
+    private static final int COL = 3;
+    private static String board[][] = new String[ROW][COL];
+    private static int move = 0;
+    private static boolean win = false;
+
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        boolean play = true;
+        String player = "";
+        move = 0;
+        do {
+            clearBoard();
+            int col;
+            int row;
+            player = "X";
+            do {
+                display();
+                col = SafeInput.getRangedInt(in, "Enter what column you want to move in", 0, 2);
+                row = SafeInput.getRangedInt(in, "Enter what column you want to move in", 0, 2);
+                board[col][row] = player;
+                System.out.println(board[col][row]);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+                if (player == "X")
+                {
+                    player = "Y";
+                }
+                else
+                {
+                    player = "X";
+                }
+            } while (isValidMove(row, col));
         }
+        while (play);
+
+        win = isWin(player);
+    }
+
+    private static void clearBoard() {
+        for (int row = 0; row < ROW; row++) {
+            for (int col = 0; col < COL; col++) {
+                board[row][col] = " ";
+            }
+        }
+    } //sets board elements to a space
+
+    private static void display() {
+        for (int row = 0; row < ROW; row++) {
+            for (int col = 0; col < COL; col++) {
+                System.out.println(board[row][col] + " | ");
+            }
+        }
+        in.nextLine();
+    } //shows tictactoe game for next movement
+
+    private static boolean isValidMove(int row, int col) {
+        return board[row][col].equals(" ");
+    }
+
+    private static boolean isWin(String player) {
+        if (isColWin(player) || isRowWin(player) || isDiagnalWin(player)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isRowWin(String player) {
+        for (int row = 0; row < ROW; row++) {
+            if (board[row][0].equals(player) && board[row][1].equals(player) && board[row][2].equals(player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isDiagnalWin(String player) {
+        for (int col = 0; col < COL; col++) {
+            for (int row = 0; row < ROW; row++) {
+                if (board[0][0].equals(player) && board[1][1].equals(player) && board[2][2].equals(player)) {
+                    return true;
+                } else if (board[0][2].equals(player) && board[1][1].equals(player) && board[2][0].equals(player)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean isColWin(String player) {
+        for (int col = 0; col < COL; col++) {
+            if (board[col][0].equals(player) && board[col][1].equals(player) && board[col][2].equals(player)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean isTie()
+    {
+        if (move == 9 && !win)
+        {
+            return true;
+        } else if (move >= 7 && !win)
+        {
+            for (int col = 0; col < COL; col++)
+            {
+                for (int row = 0; row < ROW; row++)
+                {
+                    if ((board[col][0].equals("X") && board[col][0].equals("Y") || (board[col][1].equals("X") && board[col][1].equals("Y") || (board[col][2].equals("X") && board[col][2].equals("Y") || board[0][0].equals("Y") && board[1][1].equals("X") || board[0][0].equals("X") && board[1][1].equals("Y") || board[0][2].equals("X") && board[1][1].equals("Y") || board[0][2].equals("Y") && board[1][1].equals("X")) || board[row][0].equals("X") && board[row][0].equals("Y") || (board[row][1].equals("X") && board[row][1].equals("Y") || (board[row][2].equals("X") && board[row][2].equals("Y"))))))
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
